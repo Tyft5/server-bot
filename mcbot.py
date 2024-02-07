930d16fbb5f8f5da32f031680c4130429265c579
Tyler <tyftip@gmail.com>
2024-02-07 18:47:21 -0500

    Implement xmage options

--- mcbot.py
+++ mcbot.py
@@ -1,4 +1,4 @@
-from os import scandir, getenv
+from os import scandir, getenv, path
 import subprocess
 import discord
 from discord.ext import commands
@@ -15,19 +15,24 @@ MC_PORT = getenv('MC_PORT')
 
 print("Bot started, env loaded.")
 
+
 def get_world_names(ctx: discord.AutocompleteContext) -> list[str]:
-    return [f.name for f in scandir('/opt/minecraft/') if f.is_dir()]
+    mc_worlds = [f.name for f in scandir('/opt/minecraft/') if f.is_dir()]
+    xmage = path.exists(path.dirname("/opt/xmage/"))
+    return ['XMage'] + mc_worlds if xmage else mc_worlds
 
 def get_public_ip() -> str:
     command = ['curl', 'ipinfo.io/ip']
     proc = subprocess.run(command, capture_output=True, text=True)
     return str(proc.stdout)
 
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(intents=intents)
 
 guild_ids=[321330862933278720, 905274461962530817]
+
 
 @bot.slash_command(name='start', guild_ids=guild_ids, description=START_DESC)
 async def start(
@@ -36,12 +41,19 @@ async def start(
 ):
     print('Received start command')
 
-    path = f'/opt/minecraft/{world}'
-    startmc = ("sh run.sh 2>&1 "
-              + "| sed -n -e 's/^.*There are \([0-9]*\)\/[0-9] players.*$/\1/' "
-              + """-e 't M' -e 'b' -e ": M w players" -e 'd' """
-              + """| grep -v -e "INFO" -e "Can't keep up" """)
-    command = f'screen -dmS {world} {startmc}'
+    if world == "XMage":
+        port = ""
+        path = "/opt/xmage/"
+        start_cmd = "sh startServer.sh 2>&1"
+    else:
+        port = f":{MC_PORT}"
+        path = f'/opt/minecraft/{world}'
+        start_cmd = ("sh run.sh 2>&1 "
+                + "| sed -n -e 's/^.*There are \([0-9]*\)\/[0-9] players.*$/\1/' "
+                + """-e 't M' -e 'b' -e ": M w players" -e 'd' """
+                + """| grep -v -e "INFO" -e "Can't keep up" """)
+
+    command = f'screen -dmS {world} {start_cmd}'
 
     print(f'Running command: {command}')
 
@@ -50,11 +62,11 @@ async def start(
     except Exception as e:
         print("Start command failed. Exception:")
         print(e)
-        msg = f"\U0001F534 Failed to start world {world}."
+        msg = f"\U0001F534 Failed to start server {world}."
     else:
         ip = get_public_ip()
-        msg = (f"\U0001F7E2 World {world.capitalize()} is starting "
-               + f"at address: {ip}:{MC_PORT}"
+        msg = (f"\U0001F7E2 Server {world.capitalize()} is starting "
+               + f"at address: {ip}{port}"
                + "\nPlease do not attempt to join until the server status is green."
                + "\nPlease /stop the server when you're through playing.")
 
@@ -70,14 +82,21 @@ async def stop(
     print('Received stop command')
 
     # the screen session will end when the server is stopped
-    path=f'/opt/minecraft/{world}'
-    command = f"screen -S {world} -p 0 -X stuff 'stop\15'"
+    if world == 'XMage':
+        path = f'/opt/xmage/'
+        stop_cmd = f'pkill -f "/usr/bin/java.*{path}/lib/mage-server-"\15'
+    else:
+        path = f'/opt/minecraft/{world}'
+        stop_cmd = "stop\15"
+
+    command = f"screen -S {world} -p 0 -X stuff '{stop_cmd}'"
+
     print(f'Running command: {command}')
 
     subprocess.run(command, shell=True, cwd=path)
 
     print(f'Stopped {world}')
-    msg = f"\U0001F534 World {world.capitalize()} stopped!"
+    msg = f"\U0001F534 Server {world.capitalize()} stopped!"
 
     await ctx.respond(msg)
 
@@ -104,7 +123,7 @@ async def list(ctx: discord.ApplicationContext):
     msg = ''
     ip = get_public_ip()
     if not world_names:
-        msg = 'There are no existing worlds!'
+        msg = 'There are no existing servers!'
     for world in world_names:
         if msg: msg += '\n'
         serverup = False
@@ -118,8 +137,15 @@ async def list(ctx: discord.ApplicationContext):
                 serverup = False
 
         if serverup:
-            msg += (f'\U0001F7E2 {world} is up with {num_players}'
-                    + f' player(s) at {ip}:{MC_PORT}!')
+
+            if world == 'XMage':
+                port = ""
+            else:
+                port = f":{MC_PORT}"
+
+            # msg += (f'\U0001F7E2 {world} is up with {num_players}'
+            #         + f' player(s) at {ip}:{MC_PORT}!')
+            msg += (f'\U0001F7E2 {world} is up at {ip}{port}!')
             print(f'{world} is up')
         else:
             msg += f'\U0001F534 {world} is down'
